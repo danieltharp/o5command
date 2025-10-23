3229d5918b977d44c67ea8ac9febd575823954c2
Daniel Tharp <github@nags.me>
2025-10-23 11:18:58 -0600

    Use urns rather than raw objects

--- terraform/project.tf
+++ terraform/project.tf
@@ -4,7 +4,7 @@ resource "digitalocean_project" "project" {
   purpose     = "Web Application"
   environment = "Production"
   resources   = [
-    digitalocean_kubernetes_cluster.cluster,
-    digitalocean_database_cluster.postgres
+    digitalocean_kubernetes_cluster.cluster.urn,
+    digitalocean_database_cluster.postgres.urn
   ]
 }
